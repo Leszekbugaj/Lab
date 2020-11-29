64b67ac76a2a5093611bd4f28e847330d7ff2559
Leszekbugaj <48981350+Leszekbugaj@users.noreply.github.com>
2020-11-29 09:05:22 +0100

    new

---
+++ NewBrandingStyle.Web/Database/NewBrandingDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using NewBrandingStyle.Web.Entities;
+
+namespace NewBrandingStyle.Web.Database
+{
+    public class NewBrandingDbContext : DbContext
+    { 
+        public NewBrandingDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<ItemEntity> Items { get; set; }
+        
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            
+        }
+    }
+}
